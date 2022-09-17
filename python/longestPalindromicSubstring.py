587f1fa0f787de633d055102260a8c3a8672e6fe
Blaine Lafreniere <brlafreniere@gmail.com>
2022-09-17 14:36:06 -0600

    delete some old solutions

--- python/longestPalindromicSubstring.py
+++
@@ -1,3 +0,0 @@
-class Solution:
-    def longestPalndrome(self, s: str) -> str:
-        
--- rust/scratch_space/src/main.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    let some_string = "food";
-    let i: usize = 3;
-    let byte_string = some_string.as_bytes();
-    let result = String::from_utf8(byte_string[1..2].to_vec()).ok().unwrap();
-    println!("{}", result);
-}
